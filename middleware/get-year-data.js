3b14de40a6713e310d34f18c368920ed5ae210ef
Steven Wilson <steven@dominion79.co.uk>
2019-01-04 14:11:30 +0000

    Add getYearData middleware that generates a array of empty objects.

--- middleware/get-year-data.js
+++ middleware/get-year-data.js
@@ -1,15 +1,36 @@
-yaml = require('js-yaml');
-fs   = require('fs');
+const yaml = require('js-yaml');
+const fs   = require('fs');
+const moment = require('moment');
+const year = 2019;
 
 const getYearData = (req, res, next) => {
-    console.log('Getting a year of data...')
+    console.log('Getting a year of data...');
     try {
-        const year = yaml.safeLoad(fs.readFileSync('./data/2019.yaml', 'utf8'));
-        console.log(year);
+        // const year = yaml.safeLoad(fs.readFileSync('./data/2019.yaml', 'utf8'));
+        const daysInAYear = getNumberOfDaysInAYear(year);
+        const emptyYear = buildEmptyYear(daysInAYear, year);
+        res.locals.emptyYear = emptyYear;
     } catch (e) {
         console.log(e);
     }
     next()
 }
 
-module.exports = getYearData;
+const buildEmptyYear = (daysInAYear, year) => {
+    return [...Array(daysInAYear).keys()].map(function (day) {
+        return {
+            id: day,
+            date: moment(`01/01/${year}`, 'DD/MM/YYYY', true).add(day, 'days').format('DD-MM-YYYY'),
+            rating: 0,
+            note: ''
+        }
+    });
+}
+
+const getNumberOfDaysInAYear = year => moment([year]).isLeapYear() ? 366 : 365;
+
+module.exports = {
+    getYearData,
+    getNumberOfDaysInAYear,
+    buildEmptyYear,
+};
---
+++ test/get-year-data.test.js
@@ -0,0 +1,40 @@
+const { getYearData, getNumberOfDaysInAYear, buildEmptyYear} = require('../middleware/get-year-data');
+
+describe('Check if year is a leep year', ()  => {
+    test('2020 is a leep year', () => {
+        expect(getNumberOfDaysInAYear(2020)).toBe(366);
+    });
+    test('2019 is NOT a leep year', () => {
+        expect(getNumberOfDaysInAYear(2019)).toBe(365);
+    });
+});
+
+describe('Check structure of empty year object', ()  => {
+    test('Returns two objects', () => {
+        const emptyYear = buildEmptyYear(2, 2019);
+        const mockEmptyYear = buildMockEmptyYear();
+        expect(emptyYear).toEqual(mockEmptyYear);
+    });
+    test('first object has correct structure', () => {
+        const mockEmptyYear = buildMockEmptyYear();
+        const emptyYear = buildEmptyYear(1, 2019);
+        expect(emptyYear[0]).toEqual(mockEmptyYear[0]);
+    })
+});
+
+const buildMockEmptyYear = () => {
+    return [
+        {
+            id: 0,
+            date: '01-01-2019',
+            rating: 0,
+            note: ''
+        },
+        {
+            id: 1,
+            date: '02-01-2019',
+            rating: 0,
+            note: ''
+        }
+    ]
+}
